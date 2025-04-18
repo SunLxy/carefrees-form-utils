a2f652ba2bcdbf3ec1911895baaa91ce7e8a64d3
SunLxy <1011771396@qq.com>
2025-04-18 20:35:02 +0800

    fix:修改参数

--- packages/react/src/hooks/register/register.FormList.ts
+++ packages/react/src/hooks/register/register.FormList.ts
@@ -1,32 +1,31 @@
+import { useEffect, useMemo } from 'react';
+import { RegisterFormItemOptions, useRegisterFormItem } from './register.FormItem';
+import { useFormList } from '../useFormList';
 
-import { useEffect, useMemo } from "react"
-import { RegisterFormItemOptions, useRegisterFormItem } from "./register.FormItem"
-import { useFormList } from "../useFormList"
-
-export interface RegisterFormListOptions extends RegisterFormItemOptions {
-
-}
+export interface RegisterFormListOptions extends RegisterFormItemOptions {}
 
 /**注册表单List到表单实例中*/
 export const useRegisterFormList = (options: RegisterFormListOptions) => {
-  const { ...rest } = options
-  const { ruleInstance, formItemInstance, form, newName } = useRegisterFormItem({ ...rest })
-  const formListInstance = useFormList()
+  const { ...rest } = options;
+  const { ruleInstance, formItemInstance, form, newName, parentName } = useRegisterFormItem({ ...rest });
+  const formListInstance = useFormList();
 
-  useMemo(() => formListInstance.ctor(newName), [newName])
+  useMemo(() => formListInstance.ctor(newName), [newName]);
 
-  formListInstance.instance = form
-  formListInstance.sort = options.sort
-  formListInstance.formItemInstance = formItemInstance
+  formListInstance.instance = form;
+  formListInstance.rule = ruleInstance;
+  formListInstance.sort = options.sort;
+  formListInstance.formItemInstance = formItemInstance;
+  formListInstance.parentDataField = parentName;
 
   useEffect(() => {
     const unMount = form.registerFormList(options.name, formListInstance);
     return () => unMount();
-  }, [options.name, formListInstance])
+  }, [options.name, formListInstance]);
 
   return {
     ruleInstance,
     formItemInstance,
     formListInstance,
-  }
-}
+  };
+};
